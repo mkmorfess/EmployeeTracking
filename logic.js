a6081ab93c2c546824607b2f58ff655e81263ea9
sbribrowne <sbribrowne@gmail.com>
2017-10-28 10:36:29 -0500

    add javascript file

---
+++ logic.js
@@ -0,0 +1,19 @@
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAypX9oN76rocVMGQHYiPj39q7S1jYi_bE",
+    authDomain: "employee-project-9a0d7.firebaseapp.com",
+    databaseURL: "https://employee-project-9a0d7.firebaseio.com",
+    projectId: "employee-project-9a0d7",
+    storageBucket: "employee-project-9a0d7.appspot.com",
+    messagingSenderId: "1094778755062"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  var employeename="";
+  var role="";
+  var startdate="";
+  
+</script>
